d2fc23507dbe05e7a255db74c343709aae9c5034
Luo Shenghaw <53236390+ShengHaoLo@users.noreply.github.com>
2026-03-26 07:55:13 +0800

    Create i18n.js for multilingual support
    Added multilingual dictionary for i18n support with translations in Traditional Chinese and English.

---
+++ assets/js/i18n.js
@@ -0,0 +1,95 @@
+// 多語系字典
+const i18n = {
+  "zh-TW": {
+    home: "首頁",
+    pride: "彩虹文化祭",
+    screening: "篩檢",
+    activity: "活動",
+
+    title: "自我健康管理問卷",
+    basic_info: "基本資料",
+    nickname: "暱稱",
+    agreement: "我同意提供此問卷資料作為健康管理用途",
+
+    gender: "性別認同",
+    cisman: "順性別男性",
+    ciswoman: "順性別女性",
+    transgender: "跨性別",
+    nonbinary: "非二元性別",
+
+    orientation: "性傾向",
+    heterosexual: "異性戀",
+    homosexual: "同性戀",
+    bisexual: "雙性戀",
+    orientation_others: "其他",
+
+    education: "教育程度",
+    elementary: "小學",
+    junior_high: "國中",
+    senior_high: "高中",
+    bachelor: "大學",
+    master: "碩士",
+    doctor: "博士",
+
+    residence: "居住地",
+    hsinchu_city: "新竹市",
+    hsinchu_county: "新竹縣",
+    miaoli_county: "苗栗縣",
+    taoyuan_city: "桃園市",
+    residence_others: "其他",
+
+    screening_section: "檢測紀錄",
+    last_screening: "最近一次篩檢日期",
+    screening_place: "篩檢地點",
+    screening_hiv: "HIV 篩檢",
+    screening_syphilis: "梅毒篩檢",
+
+    submit: "送出問卷"
+  },
+  "en": {
+    home: "Home",
+    pride: "Pride Festival",
+    screening: "Screening",
+    activity: "Activities",
+
+    title: "Self Health Management Questionnaire",
+    basic_info: "Basic Information",
+    nickname: "Nickname",
+    agreement: "I agree to provide this questionnaire data for health management purposes",
+
+    gender: "Gender Identity",
+    cisman: "Cisgender Male",
+    ciswoman: "Cisgender Female",
+    transgender: "Transgender",
+    nonbinary: "Non-binary",
+
+    orientation: "Sexual Orientation",
+    heterosexual: "Heterosexual",
+    homosexual: "Homosexual",
+    bisexual: "Bisexual",
+    orientation_others: "Others",
+
+    education: "Education Level",
+    elementary: "Elementary",
+    junior_high: "Junior High",
+    senior_high: "Senior High",
+    bachelor: "Bachelor",
+    master: "Master",
+    doctor: "Doctor",
+
+    residence: "Residence",
+    hsinchu_city: "Hsinchu City",
+    hsinchu_county: "Hsinchu County",
+    miaoli_county: "Miaoli County",
+    taoyuan_city: "Taoyuan City",
+    residence_others: "Others",
+
+    screening_section: "Screening Record",
+    last_screening: "Last Screening Date",
+    screening_place: "Screening Place",
+    screening_hiv: "HIV Screening",
+    screening_syphilis: "Syphilis Screening",
+
+    submit: "Submit Questionnaire"
+  }
+};
